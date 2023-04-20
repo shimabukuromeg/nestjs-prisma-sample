851da38abec63c3e855f574b622fc45f10e11038
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-04-20 11:15:28 +0900

    構造化ログ追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,60 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from "src/prisma.service";
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+const interceptors = [
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor,
+  },
+];
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  WinstonModule.forRootAsync({
+    useFactory: (configService: ConfigService) => ({
+      level: 'info',
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            severity(),
+            errorReport(),
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+      ],
+    }),
+    inject: [ConfigService],
+  }),
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, ...interceptors],
 })
 export class AppModule { }
+
+const severity = winston.format((info) => {
+  info["severity"] = info.level.toUpperCase();
+  return info;
+});
+
+const errorReport = winston.format((info) => {
+  if (info instanceof Error) {
+    info.err = {
+      name: info.name,
+      message: info.message,
+      stack: info.stack,
+    };
+  }
+  return info;
+});
---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,35 @@
+
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, LoggerService, Inject } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+
+    constructor(
+        @Inject(WINSTON_MODULE_NEST_PROVIDER)
+        private readonly logger: LoggerService,
+    ) { }
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+
+        this.logger.log(`Before...`);
+
+
+        const req = context.switchToHttp().getRequest();
+        this.logger.log("リクエスト logger");
+        this.logger.log(req);
+        console.log('リクエスト console.log', req)
+
+        const info = `${req.method} ${req.url} for ${req.ip}`;
+        this.logger.log(`Started ${info}`);
+
+        const now = Date.now();
+        return next
+            .handle()
+            .pipe(
+                tap(() => this.logger.log(`After... ${Date.now() - now}ms`)),
+            );
+    }
+}
