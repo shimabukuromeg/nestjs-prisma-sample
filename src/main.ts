ccee3bcb99f1fd7362a75dd0264d75456c1cb1bd
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-05-05 17:52:41 +0900

    トレース情報を収集するようにした

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import tracing from './tracing';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // Start SDK before nestjs factory create
+  tracing.start();
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
   const configService = app.get(ConfigService);
